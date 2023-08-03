3b40757176ed503f19f5024e5ec65f0596ab524b
uwcdc <127983572+uwcdc@users.noreply.github.com>
2023-08-03 14:15:28 -0700

    Update post_create.sh
    Add braingeneers as a postCreateCommand

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,3 +2,6 @@
 conda activate international-deploy
 conda init
 source ~/.bashrc
+
+# Install braingeneers
+python -m pip install --force-reinstall --no-cache-dir git+https://github.com/uw-ssec/braingeneerspy.git@update_repo#egg=braingeneers[data,iot,analysis,ml,hengenlab]
