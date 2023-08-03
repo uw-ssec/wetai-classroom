52f5f8845b648086148253b6fcf92ae71b1a8252
Don Setiawan <landungs@uw.edu>
2023-08-03 15:08:45 -0700

    Update post_create.sh

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,4 +1,2 @@
-# Activate Conda env
-conda activate international-deploy
-conda init
-source ~/.bashrc
+# Install braingeneers
+${NB_PYTHON_PREFIX}/bin/pip install --no-cache git+https://github.com/uw-ssec/braingeneerspy.git#egg=braingeneers[data,iot,analysis,ml,hengenlab]
