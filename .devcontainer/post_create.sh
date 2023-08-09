a493cbce05fa139450c6d242dcd3e69d1b6dda32
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2023-08-09 16:23:55 -0700

    Update post_create.sh
    Remove `jupyter` install (moving to `environment.yml`)

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,5 +2,4 @@
 ${NB_PYTHON_PREFIX}/bin/pip install --no-cache git+https://github.com/uw-ssec/braingeneerspy.git#egg=braingeneers[data,iot,analysis,ml]
 
 # Install Jupyter
-${NB_PYTHON_PREFIX}/bin/pip install jupyter
 jupyter notebook --generate-config # Create config file
