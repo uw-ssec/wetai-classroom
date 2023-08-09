92853bbebacccce10edcfc0a12841e0e1f95bb34
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2023-08-09 12:42:10 -0700

    Update post_create.sh
    Add Jupyter package and config file

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,2 +1,6 @@
 # Install braingeneers
 ${NB_PYTHON_PREFIX}/bin/pip install --no-cache git+https://github.com/uw-ssec/braingeneerspy.git#egg=braingeneers[data,iot,analysis,ml]
+
+# Install Jupyter
+${NB_PYTHON_PREFIX}/bin/pip install jupyter
+jupyter notebook --generate-config # Create config file
