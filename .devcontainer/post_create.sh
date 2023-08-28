623f38bc464afc6fae031855ee430ea973d35805
uwcdc <127983572+uwcdc@users.noreply.github.com>
2023-08-28 20:38:34 +0000

    Update .gitignore

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,2 +1,7 @@
 # Install braingeneers
 ${NB_PYTHON_PREFIX}/bin/pip install --no-cache git+https://github.com/uw-ssec/braingeneerspy.git#egg=braingeneers[data,iot,analysis,ml,hengenlab]
+
+# Install Jupyter
+python -m pip install jupyter # Install Jupyter package
+jupyter notebook notebook --generate-config # Create config file
+jupyter notebook password # Set password
